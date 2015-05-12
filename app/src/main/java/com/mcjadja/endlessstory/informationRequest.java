ddd7f2b1169079714fe7600ff216a4c9347faddb
Jadja <jadjadebeste@gmail.com>
2015-05-12 20:07:18 +0200

    Update 2.2
    main game update

--- app/src/main/java/com/mcjadja/endlessstory/informationRequest.java
+++ app/src/main/java/com/mcjadja/endlessstory/informationRequest.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 /**
  * Created by Jadja on 5/12/2015.
@@ -15,6 +16,7 @@ public class informationRequest extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.informationrequest);
     }
+
     public void doMenu(View v) {
         //Starting a new Intent
         Intent nextScreen = new Intent(getApplicationContext(), MainActivity.class);
@@ -22,9 +24,14 @@ public class informationRequest extends Activity {
         startActivity(nextScreen);
     }
     public void startPath(View v) {
+        EditText edt = (EditText)findViewById(R.id.username);
+        String name = edt.getText().toString();
         //Starting a new Intent
         Intent nextScreen = new Intent(getApplicationContext(), pathPage.class);
+        nextScreen.putExtra("name", name);
 
         startActivity(nextScreen);
+
     }
+
 }
--- app/src/main/java/com/mcjadja/endlessstory/pathPage.java
+++ app/src/main/java/com/mcjadja/endlessstory/pathPage.java
@@ -12,21 +12,52 @@ import android.widget.TextView;
  */
 public class pathPage extends Activity {
 
-    public static String path = "P";
+    String path = "P";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pathpage);
+        Intent i = getIntent();
+        i.getStringExtra("name");
+        String name = i.getStringExtra("name");
+    }
+
+    public void Choose1(View V) {
+        path+= "0";
+        String Text = "getResources().getString(R.string." + path + "_T1)";
+        String Option1 = "getResources().getString(R.string." + path + "_C1)";
+        String Option2 = "getResources().getString(R.string." + path + "_C2)";
+        TextView tv = (TextView) findViewById(R.id.Text);
+        tv.setText(Text);
+        TextView tv1 = (TextView) findViewById(R.id.button1);
+        tv1.setText(Option1);
+        TextView tv2 = (TextView) findViewById(R.id.button2);
+        tv2.setText(Option2);
+    }
+
+    public void Choose2(View V) {
+        path+= "1";
+        String Text = "getResources().getString(R.string." + path + "_T1)";
+        String Option1 = "getResources().getString(R.string." + path + "_C1)";
+        String Option2 = "getResources().getString(R.string." + path + "_C2)";
+        TextView tv = (TextView) findViewById(R.id.Text);
+        tv.setText(Text);
+        TextView tv1 = (TextView) findViewById(R.id.button1);
+        tv1.setText(Option1);
+        TextView tv2 = (TextView) findViewById(R.id.button2);
+        tv2.setText(Option2);
     }
 
     public void doStart(View v) {
-        path = "P";
+        String Text = "getResources().getString(R.string." + path + "_T1)";
+        String Option1 = "getResources().getString(R.string." + path + "_C1)";
+        String Option2 = "getResources().getString(R.string." + path + "_C2)";
         TextView tv = (TextView) findViewById(R.id.Text);
-        tv.setText("Choose a path.");
+        tv.setText(Text);
         TextView tv1 = (TextView) findViewById(R.id.button1);
-        tv1.setText("Choice 1");
+        tv1.setText(Option1);
         TextView tv2 = (TextView) findViewById(R.id.button2);
-        tv2.setText("Choice 2");
+        tv2.setText(Option2);
     }
 }
