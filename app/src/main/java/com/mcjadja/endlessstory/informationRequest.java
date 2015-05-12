79b5f681650f8496271bfb0de7974e0c1f499d57
Jadja <jadjadebeste@gmail.com>
2015-05-12 15:18:32 +0200

    Update 2.1
    background image and universal cancel code.

--- app/src/main/java/com/mcjadja/endlessstory/informationRequest.java
+++ app/src/main/java/com/mcjadja/endlessstory/informationRequest.java
@@ -1,7 +1,9 @@
 package com.mcjadja.endlessstory;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by Jadja on 5/12/2015.
@@ -12,5 +14,17 @@ public class informationRequest extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.informationrequest);
+    }
+    public void doMenu(View v) {
+        //Starting a new Intent
+        Intent nextScreen = new Intent(getApplicationContext(), MainActivity.class);
+
+        startActivity(nextScreen);
+    }
+    public void startPath(View v) {
+        //Starting a new Intent
+        Intent nextScreen = new Intent(getApplicationContext(), pathPage.class);
+
+        startActivity(nextScreen);
     }
 }
