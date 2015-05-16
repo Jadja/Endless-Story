ce79ab09f93b3d51c558b86fd0e75777416df885
Jadja <jadjadebeste@gmail.com>
2015-05-16 23:45:37 +0200

    Update 3.1
    Attempt at making the strings show up instead of the string codes

--- app/src/main/java/com/mcjadja/endlessstory/pathPage.java
+++ app/src/main/java/com/mcjadja/endlessstory/pathPage.java
@@ -14,6 +14,16 @@ public class pathPage extends Activity {
 
     String path = "P_";
     String name;
+    String Text = "T_";
+    String C1 = "C1_";
+    String C2 = "C2_";
+
+    // Path page strings:
+
+    //P
+    String T_ = "test page 1 text";
+    String C1_ = "test page 1 Choice 1";
+    String C2_ = "test page 1 Choice 2";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,16 +35,15 @@ public class pathPage extends Activity {
     }
 
     public void Choose1(View V) {
-        path+= "0";
-        String Text = "getResources().getString(R.string." + path + "_T1)";
-        String Option1 = "getResources().getString(R.string." + path + "_C1)";
-        String Option2 = "getResources().getString(R.string." + path + "_C2)";
+        Text+= "0";
+        C1+= "0";
+        C2+= "0";
         TextView tv = (TextView) findViewById(R.id.Text);
-        tv.setText("Hello, " + name + ". You chose option 1");
+        tv.setText(Text);
         TextView tv1 = (TextView) findViewById(R.id.button1);
-        tv1.setText(Option1);
+        tv1.setText(C1);
         TextView tv2 = (TextView) findViewById(R.id.button2);
-        tv2.setText(Option2);
+        tv2.setText(C2);
     }
 
     public void Choose2(View V) {
