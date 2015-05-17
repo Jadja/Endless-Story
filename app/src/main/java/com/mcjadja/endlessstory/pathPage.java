5e86e5a97ae8d575ea17476b68fdd439c4c4563f
Jadja <jadjadebeste@gmail.com>
2015-05-17 23:06:06 +0200

    Update 3.1.1
    The actual story still doesnt work. but it is now in a way that it
    _should_ work...

--- app/src/main/java/com/mcjadja/endlessstory/pathPage.java
+++ app/src/main/java/com/mcjadja/endlessstory/pathPage.java
@@ -14,16 +14,42 @@ public class pathPage extends Activity {
 
     String path = "P_";
     String name;
-    String Text = "T_";
-    String C1 = "C1_";
-    String C2 = "C2_";
+
+    String Text;
+    String C1;
+    String C2;
 
     // Path page strings:
 
-    //P
-    String T_ = "test page 1 text";
-    String C1_ = "test page 1 Choice 1";
-    String C2_ = "test page 1 Choice 2";
+    //P0
+    String T_0 = "test page choice 1";
+    String C1_0 = "test page choice 1 page, Choice 1";
+    String C2_0 = "test page choice 1 page, Choice 2";
+
+    //P1
+    String T_1 = "test page choice 2";
+    String C1_1 = "test page choice 2 page, 1 Choice 1";
+    String C2_1 = "test page choice 2 page, 1 Choice 2";
+
+    //P10
+    String T_10 = "test page choice 2";
+    String C1_10 = "test page choice 2 page, 1 Choice 1";
+    String C2_10 = "test page choice 2 page, 1 Choice 2";
+
+    //P11
+    String T_11 = "test page choice 2";
+    String C1_11 = "test page choice 2 page, 1 Choice 1";
+    String C2_11 = "test page choice 2 page, 1 Choice 2";
+
+    //P00
+    String T_00 = "test page choice 2";
+    String C1_00 = "test page choice 2 page, 1 Choice 1";
+    String C2_00 = "test page choice 2 page, 1 Choice 2";
+
+    //P01
+    String T_01 = "test page choice 2";
+    String C1_01 = "test page choice 2 page, 1 Choice 1";
+    String C2_01 = "test page choice 2 page, 1 Choice 2";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,29 +60,60 @@ public class pathPage extends Activity {
         name = i.getStringExtra("name");
     }
 
-    public void Choose1(View V) {
-        Text+= "0";
-        C1+= "0";
-        C2+= "0";
+    //Choices:
+    public void Choose1(View v) {
+        path+= "0";
+        Choose(v);
+    }
+    public void Choose2(View v) {
+        path+= "1";
+        Choose(v);
+    }
+
+    //Choose which string goes in the buttons and the text at the current path code:
+    public void Choose(View V) {
+        if(path == "P_0") {
+            T_0 = Text;
+            C1_0 = C1;
+            C2_0 = C2;
+        }
+        else if(path == "P_1") {
+            Text = T_1;
+            C1 = C1_1;
+            C2 = C2_1;
+        }
+        else if(path == "P_10") {
+            Text = T_10;
+            C1 = C1_10;
+            C2 = C2_10;
+        }
+        else if(path == "P_11") {
+            Text = T_11;
+            C1 = C1_11;
+            C2 = C2_11;
+        }
+        else if(path == "P_00") {
+            Text = T_00;
+            C1 = C1_00;
+            C2 = C2_00;
+        }
+        else if(path == "P_1") {
+            Text = T_01;
+            C1 = C1_01;
+            C2 = C2_01;
+        }
+        else    {
+            Text = path;
+            C1 = "Error";
+            C2 = "Error";
+        }
+
         TextView tv = (TextView) findViewById(R.id.Text);
         tv.setText(Text);
         TextView tv1 = (TextView) findViewById(R.id.button1);
         tv1.setText(C1);
         TextView tv2 = (TextView) findViewById(R.id.button2);
         tv2.setText(C2);
-    }
-
-    public void Choose2(View V) {
-        path+= "1";
-        String Text = "getResources().getString(R.string." + path + "_T1)";
-        String Option1 = "getResources().getString(R.string." + path + "_C1)";
-        String Option2 = "getResources().getString(R.string." + path + "_C2)";
-        TextView tv = (TextView) findViewById(R.id.Text);
-        tv.setText("Hello, " + name + ". You chose option 2");
-        TextView tv1 = (TextView) findViewById(R.id.button1);
-        tv1.setText(Option1);
-        TextView tv2 = (TextView) findViewById(R.id.button2);
-        tv2.setText(Option2);
     }
 
     public void doStart(View v) {
