2c0d0081958341644c19c6895c316d2834ed557c
Jadja <jadjadebeste@gmail.com>
2015-05-19 12:50:32 +0200

    Update 4.0
    Story is now working.

--- app/src/main/java/com/mcjadja/endlessstory/pathPage.java
+++ app/src/main/java/com/mcjadja/endlessstory/pathPage.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 /**
  * Created by Jadja on 5/12/2015.
  */
@@ -14,42 +16,9 @@ public class pathPage extends Activity {
 
     String path = "P_";
     String name;
-
     String Text;
     String C1;
     String C2;
-
-    // Path page strings:
-
-    //P0
-    String T_0 = "test page choice 1";
-    String C1_0 = "test page choice 1 page, Choice 1";
-    String C2_0 = "test page choice 1 page, Choice 2";
-
-    //P1
-    String T_1 = "test page choice 2";
-    String C1_1 = "test page choice 2 page, 1 Choice 1";
-    String C2_1 = "test page choice 2 page, 1 Choice 2";
-
-    //P10
-    String T_10 = "test page choice 2";
-    String C1_10 = "test page choice 2 page, 1 Choice 1";
-    String C2_10 = "test page choice 2 page, 1 Choice 2";
-
-    //P11
-    String T_11 = "test page choice 2";
-    String C1_11 = "test page choice 2 page, 1 Choice 1";
-    String C2_11 = "test page choice 2 page, 1 Choice 2";
-
-    //P00
-    String T_00 = "test page choice 2";
-    String C1_00 = "test page choice 2 page, 1 Choice 1";
-    String C2_00 = "test page choice 2 page, 1 Choice 2";
-
-    //P01
-    String T_01 = "test page choice 2";
-    String C1_01 = "test page choice 2 page, 1 Choice 1";
-    String C2_01 = "test page choice 2 page, 1 Choice 2";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,62 +38,36 @@ public class pathPage extends Activity {
         path+= "1";
         Choose(v);
     }
+    public void Restart(View v) {
+        path = "P_";
+    }
 
     //Choose which string goes in the buttons and the text at the current path code:
     public void Choose(View V) {
-        if(path == "P_0") {
-            T_0 = Text;
-            C1_0 = C1;
-            C2_0 = C2;
-        }
-        else if(path == "P_1") {
-            Text = T_1;
-            C1 = C1_1;
-            C2 = C2_1;
-        }
-        else if(path == "P_10") {
-            Text = T_10;
-            C1 = C1_10;
-            C2 = C2_10;
-        }
-        else if(path == "P_11") {
-            Text = T_11;
-            C1 = C1_11;
-            C2 = C2_11;
-        }
-        else if(path == "P_00") {
-            Text = T_00;
-            C1 = C1_00;
-            C2 = C2_00;
-        }
-        else if(path == "P_1") {
-            Text = T_01;
-            C1 = C1_01;
-            C2 = C2_01;
-        }
-        else    {
-            Text = path;
-            C1 = "Error";
-            C2 = "Error";
+        if (path.equals("P_000")) {
+            path = "P_1";
         }
 
+        if(path == "P_111")    {
+            Restart(V);
+        }
+        else {
+            final String packageName = getPackageName();
+
+            int resId_T = getResources().getIdentifier(path + "_T", "string", packageName);
+            Text = getResources().getString(resId_T);
+
+            int resId_C1 = getResources().getIdentifier(path + "_C1", "string", packageName);
+            C1 = getResources().getString(resId_C1);
+
+            int resId_C2 = getResources().getIdentifier(path + "_C2", "string", packageName);
+            C2 = getResources().getString(resId_C2);
+        }
         TextView tv = (TextView) findViewById(R.id.Text);
         tv.setText(Text);
         TextView tv1 = (TextView) findViewById(R.id.button1);
         tv1.setText(C1);
         TextView tv2 = (TextView) findViewById(R.id.button2);
         tv2.setText(C2);
-    }
-
-    public void doStart(View v) {
-        String Text = "getResources().getString(R.string." + path + "_T1)";
-        String Option1 = "getResources().getString(R.string." + path + "_C1)";
-        String Option2 = "getResources().getString(R.string." + path + "_C2)";
-        TextView tv = (TextView) findViewById(R.id.Text);
-        tv.setText(Text);
-        TextView tv1 = (TextView) findViewById(R.id.button1);
-        tv1.setText(Option1);
-        TextView tv2 = (TextView) findViewById(R.id.button2);
-        tv2.setText(Option2);
     }
 }
